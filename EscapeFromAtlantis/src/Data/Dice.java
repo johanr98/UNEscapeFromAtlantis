12f0bcc28d20660cda5fda909044f55b766b24d8
macastro9714 <miacastroco@unal.edu.co>
2016-10-27 22:13:51 -0500

    Clases Logic, Data y UI
    Creacion de las demas clases

--- EscapeFromAtlantis/src/Data/Dice.java
+++ EscapeFromAtlantis/src/Data/Dice.java
@@ -10,5 +10,5 @@ package Data;
  * @author macas
  */
 public class Dice {
-    
+    private String face;
 }
--- EscapeFromAtlantis/src/Logic/Effect.java
+++ EscapeFromAtlantis/src/Logic/Effect.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package UI;
+package Logic;
 
 /**
  *
  * @author macas
  */
-public class ConsoleUI {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Effect {
     
 }
